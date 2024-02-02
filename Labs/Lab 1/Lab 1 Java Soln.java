542d81873d9be28d68da855453ec741e3d7dc314
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-01 23:09:58 -0800

    Update Lab 1 Java Soln.java

--- Labs/Lab 1/Lab 1 Java Soln.java
+++ Labs/Lab 1/Lab 1 Java Soln.java
@@ -17,11 +17,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        //int[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-        //int[] arr = {5, 2, 3};
-        //int[] arr = {3, 2, 4};
-        int[] arr = {5, 3, 4, 5, 2};
-        int[] ans = twoSum(arr, 10);
-        System.out.println("[" + ans[0] + ", " + ans[1] + "]");
+        
     }
 }
