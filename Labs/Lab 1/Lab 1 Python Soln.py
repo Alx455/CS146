f99e6200220f9baae3af9656303b2f3d5769dadc
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-01 20:26:25 -0800

    Update Lab 1 Python Soln.py

--- Labs/Lab 1/Lab 1 Python Soln.py
+++ Labs/Lab 1/Lab 1 Python Soln.py
@@ -1,1 +1,16 @@
-
+def twoSum(nums, target):
+    answer = [0, 0]
+    indicies = {}
+    for i in range(len(nums)):
+        indicies[nums[i]] = i
+    for j in range(len(nums)):
+        if (((target-nums[j]) in indicies) and (indicies[target-nums[j]] != j)):
+            answer[0] = indicies[target-nums[j]]
+            answer[1] = j
+    return answer    
+    
+    
+    
+arr = [5, 2, 3]
+ans = twoSum(arr, 8)
+print(ans)
