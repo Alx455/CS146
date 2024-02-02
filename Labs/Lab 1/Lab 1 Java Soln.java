2000c130fbddbe5cdcd4acbf41a727770359c20c
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-01 20:08:31 -0800

    Update Lab 1 Java Soln.java

--- Labs/Lab 1/Lab 1 Java Soln.java
+++ Labs/Lab 1/Lab 1 Java Soln.java
@@ -1,25 +1,26 @@
 import java.util.HashMap;
 
 public class Main {
-static int[] twoSum(int[] arr, int target) {
-    int[] answer = {0, 0};
-    HashMap<Integer, Integer> sums = new HashMap<Integer, Integer>();
-    for (int i = 0; i < arr.length; i++) {
-        sums.put(arr[i], i);
-    }
-    for (int j = 0; j < arr.length; j++) {
-        if (sums.containsKey(target - arr[j]) && (sums.get(target - arr[j]) != j )) {
-            answer[0] = sums.get(target - arr[j]);
-            answer[1] = j;
+    static int[] twoSum(int[] nums, int target) {
+        int[] answer = {0, 0};
+        HashMap<Integer, Integer> indicies = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            indicies.put(nums[i], i);
         }
+        for (int j = 0; j < nums.length; j++) {
+            if (indicies.containsKey(target - nums[j]) && (indicies.get(target - nums[j]) != j )) {
+                answer[0] = indicies.get(target - nums[j]);
+                answer[1] = j;
+            }
+        }
+        return answer;
     }
-    return answer;
-}
 
     public static void main(String[] args) {
-        //int[] arr = {5, 2, 3};    target = 8, output = [0, 2]
-        //int[] arr = {3, 2, 4};    target = 6, output = [1, 2]
-        int[] arr = {5, 3, 4, 5, 2};    // target = 10, output = [3, 0]
+        //int[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+        //int[] arr = {5, 2, 3};
+        //int[] arr = {3, 2, 4};
+        int[] arr = {5, 3, 4, 5, 2};
         int[] ans = twoSum(arr, 10);
         System.out.println("[" + ans[0] + ", " + ans[1] + "]");
     }
