ed6f007e14c6fdfffa0c8882f7faecef59a6bb6e
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-05 09:16:08 -0800

    Update Hw 1 Java Soln.java

--- HW/HW1/Hw 1 Java Soln.java
+++ HW/HW1/Hw 1 Java Soln.java
@@ -1,12 +1,14 @@
 public class Main {
     static boolean palindrome(String s) {
+        if (s.isEmpty())
+            return true;
         for (int i = 0; i < s.length()/2 + 1; i++) {
             if (s.charAt(i) != s.charAt(s.length() - 1 - i))
                 return false;
         }
         return true;
     }
-  
+
     public static void main(String[] args) {
         
     }
