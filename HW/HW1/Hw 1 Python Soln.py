f8e711c7be538adb63aefdcfe9974ae4c5547db8
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-05 12:03:39 -0800

    Update Hw 1 Python Soln.py

--- HW/HW1/Hw 1 Python Soln.py
+++ HW/HW1/Hw 1 Python Soln.py
@@ -1,1 +1,8 @@
+def palindrome(target):
+    if not target:
+        return True
+    for i in range(0, len(target)//2 + 1):
+        if (target[i] != target[len(target) - 1 - i]):
+            return False
+    return True
 
