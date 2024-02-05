2ce91127042ec11e977d798a561ef1ebeb86262a
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-05 15:05:01 -0800

    Update Hw 1 Java Soln.java

--- HW/HW1/Hw 1 Java Soln.java
+++ HW/HW1/Hw 1 Java Soln.java
@@ -3,13 +3,12 @@ public class Main {
         if (s.isEmpty())
             return true;
         for (int i = 0; i < s.length()/2 + 1; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i))
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(s.length() - 1 - i)))
                 return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        
     }
 }
