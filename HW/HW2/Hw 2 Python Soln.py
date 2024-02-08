7f3315038851a2f734116e839986445ace64077d
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-07 18:32:50 -0800

    Update Hw 2 Python Soln.py

--- HW/HW2/Hw 2 Python Soln.py
+++ HW/HW2/Hw 2 Python Soln.py
@@ -1,1 +1,13 @@
-
+def first_bad_version(n):
+    left = 1
+    right = n
+    mid = None
+    prev = 0
+    while (left <= right):
+        mid = (left + right) // 2;
+        if (isBadVersion(mid)):
+            prev = mid
+            right = mid - 1
+        elif (!isBadVersion(mid)):
+            left = mid + 1
+    return prev
