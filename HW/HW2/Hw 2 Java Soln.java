3cbd564e7b73537e5a0cc7d95be916cb5469b7de
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-07 17:20:31 -0800

    Update Hw 2 Java Soln.java

--- HW/HW2/Hw 2 Java Soln.java
+++ HW/HW2/Hw 2 Java Soln.java
@@ -1,1 +1,23 @@
 
+public class Main {
+    static int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+	      int mid;
+        int prev = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (isBadVersion(mid)) {
+                prev = mid;
+		            right = mid - 1;
+            } else if (!isBadVersion(mid)) {
+                left = mid + 1;
+            }
+        }
+	      return prev;
+    }
+
+     public static void main(String[] args) {
+
+     }
+}
