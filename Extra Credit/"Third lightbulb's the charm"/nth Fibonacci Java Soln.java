66b0843894d45c0128eb6bff64aa819930e6b052
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-14 16:12:23 -0800

    Update nth Fibonacci Java Soln.java

--- Extra Credit/"Third lightbulb's the charm"/nth Fibonacci Java Soln.java
+++ Extra Credit/"Third lightbulb's the charm"/nth Fibonacci Java Soln.java
@@ -1,1 +1,19 @@
+public class Main {
+    static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        int fib = -1;
+        int beforePrev = 0;
+        int prev = 1;
+        for (int i = 1; i < n; i++) {
+            fib = beforePrev + prev;
+            beforePrev = prev;
+            prev = fib;
+        }
+        return fib;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(fibonacci(8));
+    }
+}
