ab9f4f5e550827932ed968e2e66c4add12a01108
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-14 16:22:26 -0800

    Update nth Fibonacci Python Soln.py

--- Extra Credit/"Third lightbulb's the charm"/nth Fibonacci Python Soln.py
+++ Extra Credit/"Third lightbulb's the charm"/nth Fibonacci Python Soln.py
@@ -1,1 +1,11 @@
-
+def fibonacci(n):
+    if n <= 1:
+        return n
+    fib = -1
+    beforePrev = 0
+    prev = 1
+    for i in range (1, n):
+        fib = beforePrev + prev
+        beforePrev = prev
+        prev = fib
+    return fib
