31cf2e438b8fe5401d5f00744f92948764bd483b
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-21 16:31:27 -0800

    Update Hw 6 Python Soln.py

--- HW/HW6/Hw 6 Python Soln.py
+++ HW/HW6/Hw 6 Python Soln.py
@@ -1,1 +1,12 @@
+class solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        triplets = []
+        nums.sort()
+        for i in range (0, len(nums)):
+            for j in range (i + 1, len(nums)):
+                for k in range (j + 1, len(nums)):
+                    if ((nums[i] + nums[j] + nums[k] == 0) and (triplets.count([nums[i], nums[j], nums[k]]) == 0)):
+                        triplets.append([nums[i], nums[j], nums[k]])
+        return triplets
+
 
