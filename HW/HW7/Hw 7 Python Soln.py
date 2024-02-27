36f11b6dad433c155f2def3c899012b052f7c285
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-02-26 20:36:59 -0800

    Update Hw 7 Python Soln.py

--- HW/HW7/Hw 7 Python Soln.py
+++ HW/HW7/Hw 7 Python Soln.py
@@ -1,1 +1,13 @@
+import heapq
 
+class solution:
+    def minMeetingRooms(self, intervals: list[list[int]]) -> int:   
+        runtimes = []
+        runtimes.append(intervals[0][1])
+        for i in range (1, len(intervals)):
+            if (intervals[i][0] >= runtimes[0]):
+                heapq.heappop(runtimes)
+                heapq.heappush(runtimes, intervals[i][1])
+            else:
+                heapq.heappush(runtimes, intervals[i][1])
+        return len(runtimes)
