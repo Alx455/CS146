619151902010d4933ed9054d57eda4b64e2ec245
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-03-07 19:26:45 -0800

    Update Lab 5 Java Soln.java

--- Labs/Lab 5/Lab 5 Java Soln.java
+++ Labs/Lab 5/Lab 5 Java Soln.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static boolean isValidBST(TreeNode root) {
+        if (root != null) {
+            if (root.left != null) {
+                if (root.val < root.left.val)
+                    return false;
+                if (root.left.right != null && root.val < root.left.right.val)
+                    return false;
+            }
+            if (root.right != null) {
+                if (root.val > root.right.val)
+                    return false;
+                if (root.right.left != null && root.val > root.right.left.val)
+                    return false;
+            }
+            return isValidBST(root.left) && isValidBST(root.right);
+        }
+        return true;
+    }
 
+
+    public static void main(String[] args) {
+        
+    }
+}
