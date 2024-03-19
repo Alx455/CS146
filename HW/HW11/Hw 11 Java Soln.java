ffa79cbe386d612d01cf19d7b95282f3a0e3df5a
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-03-19 16:13:59 -0700

    Update Hw 11 Java Soln.java

--- HW/HW11/Hw 11 Java Soln.java
+++ HW/HW11/Hw 11 Java Soln.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static void spreadHelper(int[][] image, int x, int y, int current, int color) {
+        if (image[x][y] == current && image[x][y] != color) {
+            image[x][y] = color;
+            if ((x - 1) >= 0)
+                spreadHelper(image, x - 1, y, current, color);
+            if ((x + 1) <= image.length - 1)
+                spreadHelper(image, x + 1, y, current, color);
+            if ((y - 1) >= 0)
+                spreadHelper(image, x, y - 1, current, color);
+            if ((y + 1) <= image[0].length - 1)
+                spreadHelper(image, x, y + 1, current, color);
+        }
+    }
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if (image[sr][sc] == color)
+            return image;
+        spreadHelper(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
 
+    public static void main(String[] args) {
+        
+    }
+}
