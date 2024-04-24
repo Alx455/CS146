55c06f937925f308475b75c13ca36684773db451
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-04-24 11:36:08 -0700

    Update Lab 8 Python Soln.py

--- Labs/Lab 8/Lab 8 Python Soln.py
+++ Labs/Lab 8/Lab 8 Python Soln.py
@@ -1,1 +1,17 @@
 
+def coinChange(coins: list[int], amount: int) -> int:
+    if amount == 0:
+        return 0
+
+    sumCounts = [float('inf')] * (amount + 1)
+    sumCounts[0] = 0
+    
+    for i in range(0, amount+1):
+        for j in range(0, len(coins)):
+            if (coins[j] <= i and sumCounts[i - coins[j]] != float('inf')):
+                if (sumCounts[i] > sumCounts[i - coins[j]]):
+                    sumCounts[i] = sumCounts[i - coins[j]] + 1
+    if sumCounts[amount] == float('inf'):
+        return -1
+    else:
+        return sumCounts[amount]
